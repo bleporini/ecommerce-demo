e82ea4f73d2e062b1b7c1001112ef8a4bf148050
Brice LEPORINI <bleporini@gmail.com>
2023-05-12 17:47:51 +0400

    Update according to new response from confluent CLI

--- get_ksqldb_api_key.sh
+++ get_ksqldb_api_key.sh
@@ -14,6 +14,6 @@ echo Creating API key for ksqlDB cluster $KSQLDB_ID
 confluent environment use $ENV_ID
 
 confluent api-key create --resource $KSQLDB_ID --description "For interacting with ksqlDB" --output json > etc/${KSQLDB_ID}_api_key.json
-cat etc/${KSQLDB_ID}_api_key.json | jq --raw-output  '"KSQLDB_API_KEY=\(.key)"' >> etc/vars.sh
-cat etc/${KSQLDB_ID}_api_key.json | jq --raw-output  '"KSQLDB_API_SECRET=\(.secret)"' >> etc/vars.sh
+cat etc/${KSQLDB_ID}_api_key.json | jq --raw-output  '"KSQLDB_API_KEY=\(.api_key)"' >> etc/vars.sh
+cat etc/${KSQLDB_ID}_api_key.json | jq --raw-output  '"KSQLDB_API_SECRET=\(.api_secret)"' >> etc/vars.sh
 
